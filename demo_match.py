12a2e9ba696ae4d0dc20f8bbb81610fe963bce2f
SaiT3E1 <92740363+SaiT3E1@users.noreply.github.com>
2022-12-09 11:36:48 -0600

    Update demo_match.py

--- demo_match.py
+++ demo_match.py
@@ -34,21 +34,23 @@ condition = st.selectbox('Please select the condition you want to look for:',ids
 
 country = st.selectbox('Please select the location you want to attend the trial:',countries)
 
-cur = conn.cursor()
-sql = """select distinct conditions.nct_id ,conditions.name,studies.last_known_status, studies.last_update_posted_date, facilities.country, facilities.city from conditions 
-join studies
-on studies.nct_id = conditions.nct_id
-join facilities
-on facilities.nct_id = studies.nct_id
-where (conditions.name = '{fcondition}' and facilities.country = '{fcountry}' and (studies.last_known_status = 'Recruiting' or studies.last_known_status = 'Active, not recruiting'))""".format(fcondition=str(condition),fcountry=str(country))
-cur.execute(sql)
-trials_ = cur.fetchall()
-cur.close()
-
-  
+if (condition != "") and (country != ""):
+  cur = conn.cursor()
+  sql = """select distinct conditions.nct_id ,conditions.name,studies.last_known_status, studies.last_update_posted_date, facilities.country, facilities.city from conditions 
+  join studies
+  on studies.nct_id = conditions.nct_id
+  join facilities
+  on facilities.nct_id = studies.nct_id
+  where (conditions.name = '{fcondition}' and facilities.country = '{fcountry}' and (studies.last_known_status = 'Recruiting' or studies.last_known_status = 'Active, not recruiting'))""".format(fcondition=str(condition),fcountry=str(country))
+  cur.execute(sql)
+  trials_ = cur.fetchall()
+  cur.close()
+ 
+ 
 trials = []
 for n in range(len(trials_)):
   trials.append(trials_[n][0])
 
-
+st.write('',trials[1])
+    
 conn.close()
